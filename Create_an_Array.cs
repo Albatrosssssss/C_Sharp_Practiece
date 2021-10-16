5769dd175845ec728f44d07b214c599c4584c925
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-17 00:21:04 +0600

    Create Create_an_Array.cs

---
+++ Create_an_Array.cs
@@ -0,0 +1,18 @@
+// Create an array of four elements, and add values later
+string[] cars = new string[4];
+
+// Create an array of four elements and add values right away 
+string[] cars = new string[4] {"Volvo", "BMW", "Ford", "Mazda"};
+
+// Create an array of four elements without specifying the size 
+string[] cars = new string[] {"Volvo", "BMW", "Ford", "Mazda"};
+
+// Create an array of four elements, omitting the new keyword, and without specifying the size
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+// Declare an array
+string[] cars;
+
+// Add values, using new
+cars = new string[] {"Volvo", "BMW", "Ford"};
+

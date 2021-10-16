0e3a8976812e2f9e11653a1e3fb02cea6ea0a5b4
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-16 21:52:47 +0600

    Create Operators.cs

---
+++ Operators.cs
@@ -0,0 +1,23 @@
+      Exm:      Same As
+=	    x = 5	    x = 5	
++=  	x += 3  	x = x + 3	
+-=	  x -= 3		x = x - 3	
+*=	  x *= 3		x = x * 3	
+/=	  x /= 3		x = x / 3	
+%=	  x %= 3		x = x % 3	
+&=	  x &= 3		x = x & 3	
+|=	  x |= 3		x = x | 3	
+^=	  x ^= 3		x = x ^ 3	
+>>=	  x >>= 3		x = x >> 3	
+<<=	  x <<= 3		x = x << 3
+
+==	 Equal to               	x == y	
+!=	 Not equal	              x != y	
+>	   Greater than            	x > y	
+<	   Less than	              x < y	
+>=	 Greater than or equal to	x >= y	
+<=   Less than or equal to	  x <= y
+
+&& 	Logical and
+|| 	Logical or
+!	  Logical not

2a232d65d7477b91215e7f65885ae81613be53a7
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-16 21:46:36 +0600

    Create Input.cs

---
+++ Input.cs
@@ -0,0 +1,23 @@
+//Get User Input
+// Type your username and press enter
+Console.WriteLine("Enter username:");
+
+// Create a string variable and get user input from the keyboard and store it in the variable
+string userName = Console.ReadLine();
+
+// Print the value of the variable (userName), which will display the input value
+Console.WriteLine("Username is: " + userName);
+
+
+//Error-start
+Console.WriteLine("Enter your age:");
+int age = Console.ReadLine();
+Console.WriteLine("Your age is: " + age);
+//The Console.ReadLine() method returns a string
+//Error-End
+
+//Solve-start
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Your age is: " + age);
+//solve-end

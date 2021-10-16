d56be004577d2d36616ba5ff2dceebef267f701b
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-16 21:57:08 +0600

    Update Operators.cs

--- Operators.cs
+++ Operators.cs
@@ -1,23 +1,23 @@
 1        Exm:              SameAs
-// =	  x = 5	    x = 5	
-// +=  	x += 3  	x = x + 3	
-// -=	  x -= 3		x = x - 3	
-// *=	  x *= 3		x = x * 3	
-// /=	  x /= 3		x = x / 3	
-// %=	  x %= 3		x = x % 3	
-// &=	  x &= 3		x = x & 3	
-// |=	  x |= 3		x = x | 3	
-// ^=	  x ^= 3		x = x ^ 3	
-// >>=	x >>= 3		x = x >> 3	
-// <<=	x <<= 3		x = x << 3
+=	  x = 5	    x = 5	
++=  	x += 3  	x = x + 3	
+-=	  x -= 3		x = x - 3	
+*=	  x *= 3		x = x * 3	
+/=	  x /= 3		x = x / 3	
+%=	  x %= 3		x = x % 3	
+&=	  x &= 3		x = x & 3	
+|=	  x |= 3		x = x | 3	
+^=	  x ^= 3		x = x ^ 3	
+>>=	x >>= 3		x = x >> 3	
+<<=	x <<= 3		x = x << 3
 
-// ==	 Equal to               	    x == y	
-// !=	 Not equal	                  x != y	
-// >	 Greater than                	x > y	
-// <	 Less than	                  x < y	
-// >=	 Greater than or equal to	    x >= y	
-// <=  Less than or equal to	      x <= y
+==	 Equal to               	    x == y	
+!=	 Not equal	                  x != y	
+>	 Greater than                	x > y	
+<	 Less than	                  x < y	
+>=	 Greater than or equal to	    x >= y	
+<=  Less than or equal to	      x <= y
 
-// && 	Logical and
-// || 	Logical or
-// !	  Logical not
+&& 	Logical and
+|| 	Logical or
+!	  Logical not

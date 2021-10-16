618b487fbefea7c6b6648aa5581b95f283d03c32
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-17 01:20:42 +0600

    Create Method_Overloading.cs

---
+++ Method_Overloading.cs
@@ -0,0 +1,20 @@
+int MyMethod(int x)
+float MyMethod(float x)
+double MyMethod(double x, double y)
+
+//Another Example
+static int PlusMethodInt(int x, int y)
+{
+  return x + y;
+}
+static double PlusMethodDouble(double x, double y)
+{
+  return x + y;
+}
+static void Main(string[] args)
+{
+  int myNum1 = PlusMethodInt(8, 5);
+  double myNum2 = PlusMethodDouble(4.3, 6.26);
+  Console.WriteLine("Int: " + myNum1);
+  Console.WriteLine("Double: " + myNum2);
+}

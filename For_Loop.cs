4046795b1912f571759765907bfb752b19f3d054
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-16 23:56:57 +0600

    Create For_Loop.cs

---
+++ For_Loop.cs
@@ -0,0 +1,20 @@
+//Syntax
+for (statement 1; statement 2; statement 3) 
+{
+  // code block to be executed
+}
+
+
+//Statement 1 is executed (one time) before the execution of the code block.
+
+//Statement 2 defines the condition for executing the code block.
+
+//Statement 3 is executed (every time) after the code block has been executed.
+
+
+//Syntax For Array
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+foreach (string i in cars) 
+{
+  Console.WriteLine(i);
+}

778ac6bd1cbbe12d13326941144ee9e718266be5
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-16 21:32:23 +0600

    Create Type_Casting.cs

---
+++ Type_Casting.cs
@@ -0,0 +1,5 @@
+//Implicit Casting (automatically) - converting a smaller type to a larger type size
+//char -> int -> long -> float -> double
+
+//Explicit Casting (manually) - converting a larger type to a smaller size type
+//double -> float -> long -> int -> char

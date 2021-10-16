669278a0205242f5e5b1f0257f9142686301c64b
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-16 23:53:33 +0600

    Create While_Loop.cs

---
+++ While_Loop.cs
@@ -0,0 +1,26 @@
+//While loop
+int i = 0;
+while (i < 5) 
+{
+  Console.WriteLine(i);
+  i++;
+}
+
+
+//Do While Loop
+
+//Syntax
+do 
+{
+  // code block to be executed
+}
+while (condition);
+
+//Exm:
+int i = 0;
+do 
+{
+  Console.WriteLine(i);
+  i++;
+}
+while (i < 5);

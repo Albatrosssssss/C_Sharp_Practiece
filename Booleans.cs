b2b75fe5017fd58053181685e05dd24981f1a35a
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-16 23:37:23 +0600

    Create Booleans.cs

---
+++ Booleans.cs
@@ -0,0 +1,5 @@
+//A boolean type is declared with the bool keyword and can only take the values true or false:
+bool isCSharpFun = true;
+bool isFishTasty = false;
+Console.WriteLine(isCSharpFun);   // Outputs True
+Console.WriteLine(isFishTasty);   // Outputs False

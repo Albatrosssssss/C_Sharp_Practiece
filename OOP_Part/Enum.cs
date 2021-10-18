496e896fd189b7dae1390231664414a1ba8c0f33
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-18 16:24:25 +0600

    Update Enum.cs

--- OOP_Part/Enum.cs
+++ OOP_Part/Enum.cs
@@ -17,3 +17,24 @@ static void Main(string[] args)
   Console.WriteLine(myNum);
 }
 //output"3"
+
+
+
+//You can also assign your own enum values,
+//and the next items will update the number accordingly
+enum Months
+{
+  January,    // 0
+  February,   // 1
+  March=6,    // 6
+  April,      // 7
+  May,        // 8
+  June,       // 9
+  July        // 10
+}
+
+static void Main(string[] args)
+{
+  int myNum = (int) Months.April;
+  Console.WriteLine(myNum);
+}

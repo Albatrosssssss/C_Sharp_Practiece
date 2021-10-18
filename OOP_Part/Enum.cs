3ea8bfc6bc363ffb7fe80173c91172c18f917d3b
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-18 16:21:55 +0600

    Create Enum.cs

---
+++ OOP_Part/Enum.cs
@@ -0,0 +1,19 @@
+//An enum is a special "class" that 
+//represents a group of constants (unchangeable/read-only variables).
+enum Months
+{
+  January,    // 0
+  February,   // 1
+  March,      // 2
+  April,      // 3
+  May,        // 4
+  June,       // 5
+  July        // 6
+}
+
+static void Main(string[] args)
+{
+  int myNum = (int) Months.April;
+  Console.WriteLine(myNum);
+}
+//output"3"

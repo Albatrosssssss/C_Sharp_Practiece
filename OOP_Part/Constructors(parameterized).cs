25242d8e0e89a08a2eec64bff068a028e8fc2d10
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-18 11:50:41 +0600

    Create Constructors(parameterized).cs

---
+++ OOP_Part/Constructors(parameterized).cs
@@ -0,0 +1,45 @@
+//Program class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "Red", 1969);
+            Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+        }
+    }
+}
+
+
+
+//car class
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Car
+    {
+        public string model;
+        public string color;
+        public int year;
+
+        // Create a class constructor with multiple parameters
+        public Car(string modelName, string modelColor, int modelYear)
+        {
+            model = modelName;
+            color = modelColor;
+            year = modelYear;
+        }
+    }
+}

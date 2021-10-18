c12dbada1030a340ac474c9c454d0bc901e611c0
Sadi Al huda <67068215+binarybeast004@users.noreply.github.com>
2021-10-18 10:38:18 +0600

    Create Constructors.cs

---
+++ OOP_Part/Constructors.cs
@@ -0,0 +1,3 @@
+A constructor is a special method that is used to initialize objects. 
+The advantage of a constructor, is that it is called when an object of a class is created. 
+It can be used to set initial values for fields
